ee09feed99e95ebcc8f0e6e3389fba46fe323a90
Supreeth.M <85946079+Supreeth2319@users.noreply.github.com>
2022-06-02 21:02:00 +0530

    Create printing pattern2.c
    If the value of n=6 the output will be
    1 2 3 4 5
    1 2 3 4 5
    1 2 3 4 5
    1 2 3 4 5
    1 2 3 4 5

---
+++ printing pattern2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int n;
+    printf("enter the value of N");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+}
